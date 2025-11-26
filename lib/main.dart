2c7227af23260de9d5ac362012fddd05da5470f0
Ubuntu <ubuntu@ip-172-31-2-1.ap-northeast-1.compute.internal>
2025-11-26 08:01:17 +0000

    Remove unused Firebase init to fix Web crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 import 'core/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // Firebase is not configured for Web yet, and not used by AuthProvider/NotificationService currently.
+  // await Firebase.initializeApp(); 
   await NotificationService.instance.init();
   runApp(const FireSafetyInspectorApp());
 } 
